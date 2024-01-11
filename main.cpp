1eb1736ccffcb933cbae4e60b0996742ed63fcc6
Dylan <150361945+DumbNoxx@users.noreply.github.com>
2024-01-11 00:07:54 -0400

    Space Dasher v0.1.2-alpha
    -Tutorial 1 pre creado
    °Hay colisión en el primer triangulo rectángulo
    °Ya hay plata forma de aterrizaje
    °Se refino la nave para que la colisión sea más precisa
    
    -Para terminar de crear el tutorial 1
    °Falta arreglar el aterrizaje
    °Falta agregarle la colisión en el triangulo rectángulo y al triangulo
    
    Quisiera que me mandaran un mensaje a ver si el error de cerrado ya no esta o si sigue estando, ya que en mi equipo si funciona.
    
    un saludo acorde, nos vemos. <3

--- main.cpp
+++ main.cpp
@@ -15,15 +15,6 @@ bool gameOver(float,float,BITMAP*,int,bool);//Game over
 bool aterrizar(float,float,float,float,BITMAP*,int,bool);//Funcion para aterrizar
 bool choque(float,float,float,float,float,float,float,float);//Hace la que la nave choque con los obstaculos
 bool choqueNave(int,float,float);//Hace que la nave choque
-void botonClose(){
-	 int result = alert("Salir","Seguro que quieres salir?", NULL,"&SI","&No",'s','n');
-	 bool comprobar = false;
-	 if(result == 1){
-	 		   comprobar = true;
-				} else {
-				  	   comprobar = false;
-						 }
-	 }	
 
 
 
@@ -37,7 +28,7 @@ int main() {
 	depth = desktop_color_depth();
 	if (depth == 0) depth = 64;//Selecciona 64 bits(en tal caso ser 64 bits)
 	set_color_depth(depth);
-	set_window_close_button(botonClose());
+	
 	res = set_gfx_mode(GFX_AUTODETECT_WINDOWED, 740, 500, 0, 0);/*Es el tamanio
 		  											 	  	 	de la ventana*/	 	
 	if (res != 0) {												
